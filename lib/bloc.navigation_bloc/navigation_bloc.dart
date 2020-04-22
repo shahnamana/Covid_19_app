96355c636370f76ef1aa9e101a45f1fda28eb8b6
Naman Shah <naman.as@somaiya.edu>
2020-04-22 22:40:39 +0530

    Some changes are still left

---
+++ lib/bloc.navigation_bloc/navigation_bloc.dart
@@ -0,0 +1,34 @@
+import 'package:bloc/bloc.dart';
+import '../pages/homepage.dart';
+import '../pages/myaccountspage.dart';
+import '../pages/myorderspage.dart';
+
+import '../pages/homepage.dart';
+
+enum NavigationEvents {
+  HomePageClickedEvent,
+  MyAccountClickedEvent,
+  MyOrdersClickedEvent,
+}
+
+abstract class NavigationStates {}
+
+class NavigationBloc extends Bloc<NavigationEvents, NavigationStates> {
+  @override
+  NavigationStates get initialState => HomePage();
+
+  @override
+  Stream<NavigationStates> mapEventToState(NavigationEvents event) async* {
+    switch (event) {
+      case NavigationEvents.HomePageClickedEvent:
+        yield HomePage();
+        break;
+      case NavigationEvents.MyAccountClickedEvent:
+        yield MyAccountsPage();
+        break;
+      case NavigationEvents.MyOrdersClickedEvent:
+        yield MyOrdersPage();
+        break;
+    }
+  }
+}
